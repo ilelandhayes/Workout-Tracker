6acb35c1d6a8c6d5e80bc4650c5b9b3e93d31fa8
Leland Hayes <lelandkhayes13@gmail.com>
2021-10-10 21:04:37 -0400

    added other files needed, also updated server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const routes = require('./controllers');
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
