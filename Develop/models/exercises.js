9d1bf1e97025e939084591806165552ff8a59886
Leland Hayes <lelandkhayes13@gmail.com>
2021-10-10 21:36:29 -0400

    added exercise model

---
+++ Develop/models/exercises.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Shema;
+
+const WorkoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now,
+        },
+        exercise: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Enter exercise type!",
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter exercise name!",
+                },
+                duration: {
+                    type: Number,
+                    required: "Enter exercise duration!",
+                },
+                distance: {
+                    type: Number,
+                },
+                weight: {
+                    type: Number,
+                },
+                sets: {
+                    type: Number,
+                },
+                reps: {
+                    type: Number,
+                },
+            },
+        ],
+    },
+);
+
+
+const Workout = mongoose.model("workout", WorkoutSchema);
+
+module.exports = Workout;
