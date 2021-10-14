3b7d8a575aa0b80705d52bfdc1abb5e5a41f4bc6
Leland Hayes <lelandkhayes13@gmail.com>
2021-10-14 11:01:20 -0400

    updated routes to add and delete workouts

--- Develop/controllers/api/workoutRoutes.js
+++ Develop/controllers/api/workoutRoutes.js
@@ -1,15 +1,43 @@
 const router = require("express").Router();
-const db = require("../../models/exercises");
+const db = require("../../models");
 
-router.get("/api/exercise", (req, res) => {
-    db.find({})
-    .sort({ date: -1 })
-    .then(dbWorkouts => {
-        res.json(dbWorkouts);
-    })
-    .catch(err => {
-        res.status(400).json(err);
-    });
+router.get("/", async (req, res) => {
+    try {
+        const workouts = await db.workouts.find({});
+        res.status(200).json(workouts);
+    } catch (err) {
+        err && res.status(500).json(err);
+    }
+});
+
+router.post('/', async ({ body }, res) => {
+    try {
+        const newWorkout = await db.Workout.create(body);
+        res.status(200).json(newWorkout);
+    } catch (err) {
+        err && res.status(500).json(err);
+    }
+});
+
+router.put('/:id', async ({ params, body }, res) => {
+    try {
+        const updatedWorkout = await db.Workout.findByIdAndUpdate(
+            params.id,
+            { $push: { exercises: body } },
+            { new: true }
+        );
+        res.status(200).json(updatedWorkout);
+    } catch (err) {
+        err && res.status(500).json(err);
+    }
+});
+
+router.get('/range', (req, res) => {
+    try {
+
+    } catch (err) {
+        err && res.status(500).json(err);
+    }
 });
 
 module.exports = router;
